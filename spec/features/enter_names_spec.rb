768b6719c694b0f9a08d36980226903143d5a0b4
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-08 22:16:05 +0000

    added feature test: player loses HP

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'players can enter their names on the screen' do
   scenario 'inputted names are displayed' do
-    visit('/')
-    fill_in 'player_1', with: 'Josue'
-    fill_in 'player_2', with: 'Hannah'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content("Josue vs. Hannah")
   end
 end
